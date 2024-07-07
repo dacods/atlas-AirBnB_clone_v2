7aa464b98ed5939242933417623e3a164ff4145c
dacods <dacods.21@gmail.com>
2024-07-07 16:48:27 -0500

    Created the correct files

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -32,6 +32,10 @@ def number(n):
     return f"{n} is a number"
 
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_templates(n):
+    return render_template('number.html', n=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
