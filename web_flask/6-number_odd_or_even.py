40293c1062f5017101456770294d81220ebd6a06
dacods <dacods.21@gmail.com>
2024-07-07 17:10:21 -0500

    Fixed PEP8

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -41,7 +41,7 @@ def number_templates(n):
 def number_odd_or_even(n):
     odd_or_even = "even" if n % 2 == 0 else "odd"
     return render_template('6-number_odd_or_even.html',
-            n=n, odd_or_even=odd_or_even)
+                n=n, odd_or_even=odd_or_even)
 
 
 if __name__ == "__main__":
