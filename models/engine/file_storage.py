20dc2ae1be79017476f9ad4cfd34ef625c534c8d
dacods <dacods.21@gmail.com>
2024-07-08 18:25:27 -0500

    Fixing PEP8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,8 +55,8 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
-            FileNotFoundError
+        except FileNotFoundError:
+            pass
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
