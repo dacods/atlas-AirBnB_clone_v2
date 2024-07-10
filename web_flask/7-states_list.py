efbf407c9324d82417149746008eb52e349d70ba
dacods <dacods.21@gmail.com>
2024-07-10 17:29:13 -0500

    Made changes in 7-state_list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,13 +10,13 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def state_list():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    states = list(storage.all(State).values())
+    states.sort(key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
 
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
