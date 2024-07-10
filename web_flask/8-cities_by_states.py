302fa3a99e1539a56aa71706b6bf3e3299b5f015
dacods <dacods.21@gmail.com>
2024-07-09 19:00:48 -0500

    Created 8-cities_by_states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script that starts a flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_state():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    for state in sorted_states:
+        state.cities = sorted(state.cities, key=lambda city: city.name)
+    return render_template('8-cities_by_states.html', states=sorted_states)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
